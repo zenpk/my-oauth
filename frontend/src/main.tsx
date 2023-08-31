6b6fdfebd61bfc35551985ffdf4e931733d473ef
zenpk <99558412+zenpk@users.noreply.github.com>
2023-08-31 22:24:09 +0800

    update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./main.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Admin } from "./Admin.tsx";
 import { Login } from "./Login.tsx";
